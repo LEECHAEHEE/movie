7b246eca544fcaefe4c3cce362612c90afa993c2
FULLNAM-0UM2HQF\Administrator <leechaehee@naver.com>
2018-05-30 15:18:38 +0900

    프리뷰 슬라이더 개선중

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,4 +1,5 @@
 package test;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -8,127 +9,102 @@ import javax.swing.SwingUtilities;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 import uk.co.caprica.vlcj.player.MediaPlayer;
-import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
-import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
 
-public class Test {
+//public class Test {
+//
+//    private final JFrame frame;
+//
+//    private final EmbeddedMediaPlayerComponent component;
+//    private final EmbeddedMediaPlayer player;
+//    
+//    public static void main(String[] args) {
+//        new NativeDiscovery().discover();
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                new Test("https://youtu.be/QKWlXvt9x7c");
+//            }
+//        });
+//    }
+//
+//    public Test(String args) {
+//        frame = new JFrame("My First Media Player");
+//        frame.setBounds(500, 200, 800, 500);
+//        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+//        frame.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//            	component.release();
+//            	System.exit(0);
+//            }
+//        });
+//        
+//        component = new EmbeddedMediaPlayerComponent();
+//        player = component.getMediaPlayer();
+//        player.addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+//        	@Override
+//        	public void finished(MediaPlayer mediaPlayer) {
+//        		component.release();
+//        		frame.remove(component);
+//        	}
+//        });
+//        frame.setContentPane(component);
+//        frame.setVisible(true);
+//        
+//        player.setPlaySubItems(true);
+//        player.playMedia(args);
+//    }
+//}
 
-    private final JFrame frame;
 
-    private final EmbeddedMediaPlayerComponent component;
-    private final EmbeddedMediaPlayer player;
+public class Test extends JFrame{
+	boolean flag=true;
+	
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
     
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         new NativeDiscovery().discover();
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Test("https://youtu.be/yKX2VrYpCp8");
+                new Test("https://www.youtube.com/watch?v=zU5hM4tvkTA");
             }
         });
     }
 
     public Test(String args) {
-        frame = new JFrame("My First Media Player");
-        frame.setBounds(500, 200, 800, 500);
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
+    	super("Preview");
+        setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-1280)/2, (Toolkit.getDefaultToolkit().getScreenSize().height-720)/2, 1280, 720);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-            	component.release();
-            	System.exit(0);
+                mediaPlayerComponent.release();
+                System.exit(0);
             }
         });
         
-        component = new EmbeddedMediaPlayerComponent();
-        player = component.getMediaPlayer();
-        player.addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent() {
         	@Override
         	public void finished(MediaPlayer mediaPlayer) {
-        		component.release();
-        		frame.remove(component);
+        		if(flag) {
+        			flag=!flag;
+        		}else {
+        			dispose();
+        			mediaPlayerComponent.release();
+        			System.out.println("finished");
+        		}
         	}
-        });
-        frame.setContentPane(component);
-        frame.setVisible(true);
+        };
         
-        player.setPlaySubItems(true);
-        player.playMedia(args);
+        setContentPane(mediaPlayerComponent);
+        setVisible(true);
+        mediaPlayerComponent.getMediaPlayer().setPlaySubItems(true);
+        mediaPlayerComponent.getMediaPlayer().playMedia(args);
     }
 }
-
-//public class Test extends JPanel{
-//	private EmbeddedMediaListPlayerComponent component;
-//	private EmbeddedMediaPlayer player;
-//	
-//	public Test() {
-//		boolean found = new NativeDiscovery().discover();
-//		System.out.println(found);
-//		System.out.println(LibVlc.INSTANCE.libvlc_get_version());
-//		component = new EmbeddedMediaListPlayerComponent();
-//		player = component.getMediaPlayer();
-//		
-//		setLayout(new BorderLayout());
-//		setPreferredSize(new Dimension(640, 480));
-//		setVisible(true);
-//		add(component,BorderLayout.CENTER);
-//		player.addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
-//			@Override
-//			public void finished(MediaPlayer mediaPlayer) {
-//				endVideo();
-//			}
-//		});
-//	}//Test()
-//
-//	private void runVideo(){
-//		new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				player.playMedia("");
-//			}
-//		}).start();
-//	}
-//	
-//	private void endVideo() {
-//		component.release();
-//		remove(component);
-//	}
-//}
-
-//public class Test {
-//	 private final JFrame frame;
-//
-//	    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
-//
-//	    public static void main(final String[] args) {
-//	        new NativeDiscovery().discover();
-//	        SwingUtilities.invokeLater(new Runnable() {
-//	            @Override
-//	            public void run() {
-//	                new Test("https://youtu.be/yKX2VrYpCp8");
-//	            }
-//	        });
-//	    }
-//
-//	    public Test(String args) {
-//	        frame = new JFrame("My First Media Player");
-//	        frame.setBounds(100, 100, 1280, 720);
-//	        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-//	        frame.addWindowListener(new WindowAdapter() {
-//	            @Override
-//	            public void windowClosing(WindowEvent e) {
-//	                mediaPlayerComponent.release();
-//	                System.exit(0);
-//	            }
-//	        });
-//	        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-//	        frame.setContentPane(mediaPlayerComponent);
-//	        frame.setVisible(true);
-//	        mediaPlayerComponent.getMediaPlayer().setPlaySubItems(true);
-//	        mediaPlayerComponent.getMediaPlayer().playMedia(args);
-//	    }
-//}
 
 
 
